2fa086519dcd91cb69150ed2d7a525bc94430e60
novalhidayat041 <mnovalhidayat298@gmail.com>
2022-06-23 15:21:31 +0700

    personal access token

--- app/src/main/java/ac/id/polbeng/githubprofile/helpers/Config.kt
+++ app/src/main/java/ac/id/polbeng/githubprofile/helpers/Config.kt
@@ -1,9 +1,12 @@
 package ac.id.polbeng.githubprofile.helpers
 
+import ac.id.polbeng.githubprofile.BuildConfig
+
 class Config {
     companion object {
         const val SPLASH_SCREEN_DELAY:Long = 3000
         const val BASE_URL ="https://api.github.com"
         const val DEFAULT_USER_LOGIN = "novalhidayat041"
+        const val PERSONAL_ACCESS_TOKEN = "token ${BuildConfig.ACCESS_TOKEN}"
     }
 }
--- app/src/main/java/ac/id/polbeng/githubprofile/services/GithubUserService.kt
+++ app/src/main/java/ac/id/polbeng/githubprofile/services/GithubUserService.kt
@@ -3,10 +3,13 @@ package ac.id.polbeng.githubprofile.services
 import ac.id.polbeng.githubprofile.models.GithubUser
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
+
 interface GithubUserService {
     @GET("users/{login}")
     fun loginUser(
+        @Header("Authorization") token: String,
         @Path("login") userLogin: String
     ): Call<GithubUser>
 }
